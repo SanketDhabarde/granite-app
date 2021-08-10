b0b35bd9276dcce8ebb72f043577153a1aa422ae
Sanket Dhabarde <sanketdhabarde@gmail.com>
2021-08-10 20:48:54 +0530

    Added migration script for creating tasks table

---
+++ db/migrate/20210810151802_create_tasks.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.text :title
+      t.timestamps
+    end
+  end
+end
