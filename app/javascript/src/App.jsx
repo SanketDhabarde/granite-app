918d5d74132cd1aeb3ff0766fc31e63099d23106
Sanket Dhabarde <sanketdhabarde@gmail.com>
2021-08-11 13:28:00 +0530

    Added ability to display list of tasks

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
+import Dashboard from "components/Dashboard";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -14,8 +15,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
   );
---
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { isNil, isEmpty, either } from "ramda";
+
+import Container from "components/Container";
+import Table from "components/Tasks/Table";
+import PageLoader from "components/PageLoader";
+import tasksApi from "apis/tasks";
+
+const Dashboard = ({ history }) => {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await tasksApi.list();
+      setTasks(response.data.tasks);
+      setLoading(false);
+    } catch (error) {
+      logger.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="w-screen h-screen">
+        <PageLoader />
+      </div>
+    );
+  }
+
+  if (either(isNil, isEmpty)(tasks)) {
+    return (
+      <Container>
+        <h1 className="text-xl leading-5 text-center">
+          You have no tasks assigned 😔
+        </h1>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Table data={tasks} />
+    </Container>
+  );
+};
+
+export default Dashboard;
